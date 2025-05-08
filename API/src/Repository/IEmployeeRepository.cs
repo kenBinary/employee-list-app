31c76ce8c831bc6146a95431a867a17a6d6e338b
kenBinary <kennethbecaro0@gmail.com>
2025-05-09 03:41:07 +0800

    feat: Define Interface for employee management

--- API/src/Repository/IEmployeeRepository.cs
+++ API/src/Repository/IEmployeeRepository.cs
@@ -5,5 +5,9 @@ namespace API.src.Repository
     public interface IEmployeeRepository
     {
         public Task<Employee?> GetEmployee(int id);
+        public Task<Employee[]?> GetEmployees();
+        public Task<Employee?> AddEmployee(Employee employee);
+        public Task<Employee?> RemoveEmployee(int id);
+        public Task<Employee?> UpdateEmployee(int id, Employee employee);
     }
 }
