c8bedc322038236f2700e6eecfb4e97df593afe0
kenBinary <kennethbecaro0@gmail.com>
2025-05-09 02:59:34 +0800

    feat: Seed initial employee data in ApplicationDbContext

--- API/src/Data/ApplicationDbContext.cs
+++ API/src/Data/ApplicationDbContext.cs
@@ -11,5 +11,36 @@ namespace API.src.Data
 
         public DbSet<Employee> Employees { get; set; }
 
+        protected override void OnModelCreating(ModelBuilder modelBuilder)
+        {
+            base.OnModelCreating(modelBuilder);
+
+            modelBuilder.Entity<Employee>().HasData(
+                new Employee
+                {
+                    Id = 1,
+                    FirstName = "Juan",
+                    LastName = "Dela Cruz",
+                    Email = "JCruz@gmail.com.com",
+                    Position = "Software Developer"
+                },
+                new Employee
+                {
+                    Id = 2,
+                    FirstName = "Mark",
+                    LastName = "Henry",
+                    Email = "MHenry@gmail.com.com",
+                    Position = "Database Administrator"
+                },
+                new Employee
+                {
+                    Id = 3,
+                    FirstName = "Michael",
+                    LastName = "Water",
+                    Email = "MWater@example.com",
+                    Position = "QA Engineer"
+                }
+            );
+        }
     }
 }
