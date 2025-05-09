b82aa76e08a2ada0d7ca66352a9d3b4838b33a4c
kenBinary <kennethbecaro0@gmail.com>
2025-05-09 18:19:41 +0800

    feat: integrate React Query for data fetching in App component

--- UI/src/App.tsx
+++ UI/src/App.tsx
@@ -1,15 +1,33 @@
-import { useState } from "react";
-import { employees_7 } from "./__mocks__/employees";
+import { useEffect, useState } from "react";
 import "./App.css";
-import clsx from "clsx";
-
-interface Pagination {
-  currentPage: number;
-  totalPages: number;
-}
+import { useQuery } from "@tanstack/react-query";
+import type { Employee } from "./types/employee";
+import { fetchEmployees } from "./services/fetchEmployee";
+import { Header } from "./components/Header";
+import { SearchBar } from "./components/SearchBar";
+import { EmployeeTable } from "./components/EmployeeTable";
+import type { Pagination } from "./types/pagination";
+import { PaginationActions } from "./components/PaginationActions";
 
 function App() {
-  let employees = employees_7;
+  let employees: Employee[] = [];
+
+  const { data, isPending, error, isError } = useQuery({
+    queryKey: ["employees"],
+    queryFn: fetchEmployees,
+    refetchOnWindowFocus: false,
+  });
+
+  useEffect(() => {
+    if (!isPending && !isError) {
+      setPagination((prev) => ({
+        ...prev,
+        totalPages: Math.ceil(data.length / 5),
+      }));
+    }
+  }, [data, isPending, isError]);
+
+  employees = data || [];
 
   const [pagination, setPagination] = useState<Pagination>({
     currentPage: 1,
@@ -26,146 +44,39 @@ function App() {
     }));
   };
 
-  employees = employees.slice(
-    (pagination.currentPage - 1) * 5,
-    pagination.currentPage * 5
-  );
+  if (isPending) {
+    return (
+      <div className="flex justify-center items-center h-dvh">Loading...</div>
+    );
+  }
+  if (isError) {
+    return (
+      <div className="flex justify-center items-center h-dvh">
+        <p className="text-red-500">Error: {JSON.stringify(error)}</p>
+      </div>
+    );
+  }
+
+  if (data) {
+    employees = data.slice(
+      (pagination.currentPage - 1) * 5,
+      pagination.currentPage * 5
+    );
+  }
 
   return (
-    <main className="flex flex-col justify-center items-center h-dvh">
-      <div className="flex  justify-between w-1/2 p-5 border-b border-slate-400">
-        <p className="text-3xl font-bold"> Employee Management</p>
-        <button
-          className="btn btn-info"
-          onClick={() =>
-            (
-              document.getElementById("my_modal_3") as HTMLDialogElement
-            )?.showModal()
-          }
-        >
-          Add Employee
-        </button>
-        <dialog id="my_modal_3" className="modal">
-          <div className="modal-box">
-            <form method="dialog">
-              <button className="btn btn-sm btn-circle btn-ghost absolute right-2 top-2">
-                ✕
-              </button>
-            </form>
-            <h3 className="font-bold text-lg">Add a new Employee!</h3>
-            <div className="flex flex-col gap-4 mt-4">
-              <input
-                type="text"
-                placeholder="Enter First Name"
-                className="input w-full"
-              />
-              <input
-                type="text"
-                placeholder="Enter Last Name"
-                className="input w-full"
-              />
-              <input
-                type="email"
-                placeholder="Ente Email"
-                className="input w-full"
-              />
-              <input
-                type="text"
-                placeholder="Enter Position"
-                className="input w-full"
-              />
-            </div>
+    <main className="flex flex-col justify-center items-center h-dvh relative">
+      <Header></Header>
 
-            <button className="btn btn-info btn-sm mt-4">Add Employee</button>
-          </div>
-        </dialog>
-      </div>
-
-      <div className="w-1/2 p-5 flex">
-        <div className="join w-full">
-          <div className="w-full">
-            <label className="input validator join-item w-full">
-              <svg
-                className="h-[1em] opacity-50"
-                xmlns="http://www.w3.org/2000/svg"
-                viewBox="0 0 24 24"
-              >
-                <g
-                  strokeLinejoin="round"
-                  strokeLinecap="round"
-                  strokeWidth="2.5"
-                  fill="none"
-                  stroke="currentColor"
-                >
-                  <circle cx="11" cy="11" r="8"></circle>
-                  <path d="m21 21-4.3-4.3"></path>
-                </g>
-              </svg>
-              <input
-                type="search"
-                placeholder="Search for an Employee by ther ID"
-                required
-              />
-            </label>
-            <div className="validator-hint hidden">Enter a valid ID</div>
-          </div>
-          <button className="btn btn-info join-item">Search</button>
-        </div>
-      </div>
+      <SearchBar></SearchBar>
 
       <div className="w-1/2 flex flex-col gap-4 justify-center">
-        <div className="overflow-x-auto w-full">
-          <table className="table overflow-y-auto">
-            <thead>
-              <tr>
-                <th>ID</th>
-                <th>Name</th>
-                <th>Email</th>
-                <th>Position</th>
-              </tr>
-            </thead>
-            <tbody>
-              {employees.map((employee) => (
-                <tr key={employee.id}>
-                  <th>{employee.id}</th>
-                  <td>{employee.name}</td>
-                  <td>{employee.email}</td>
-                  <td>{employee.phone}</td>
-                </tr>
-              ))}
-            </tbody>
-          </table>
-        </div>
+        <EmployeeTable employees={employees}></EmployeeTable>
 
-        <div className="join justify-center">
-          <button
-            className="join-item btn"
-            onClick={() => {
-              handlePageChange(pagination.currentPage - 1);
-            }}
-          >
-            «
-          </button>
-          {new Array(pagination.totalPages).fill(0).map((_, index) => (
-            <button
-              key={index}
-              className={clsx("join-item btn", {
-                "btn-active": pagination.currentPage === index + 1,
-              })}
-              onClick={() => handlePageChange(index + 1)}
-            >
-              {index + 1}
-            </button>
-          ))}
-          <button
-            className="join-item btn"
-            onClick={() => {
-              handlePageChange(pagination.currentPage + 1);
-            }}
-          >
-            »
-          </button>
-        </div>
+        <PaginationActions
+          pagination={pagination}
+          handlePageChange={handlePageChange}
+        ></PaginationActions>
       </div>
     </main>
   );
