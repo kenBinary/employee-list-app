48ddc4bac459521bc017700065492cb2b895e2af
kenBinary <kennethbecaro0@gmail.com>
2025-05-09 18:16:27 +0800

    feat: add EmployeeTable component to display employee data

---
+++ UI/src/components/EmployeeTable.tsx
@@ -0,0 +1,43 @@
+import type { Employee } from "../types/employee";
+import { EditButton } from "./EditButton";
+import { DeleteButton } from "./DeleteButton";
+
+interface EmployeeTableProps {
+  employees: Employee[];
+}
+export function EmployeeTable({ employees }: EmployeeTableProps) {
+  return (
+    <div className="overflow-x-auto w-full">
+      <table className="table overflow-y-auto">
+        <thead>
+          <tr>
+            <th>ID</th>
+            <th>Name</th>
+            <th>Email</th>
+            <th>Position</th>
+            <th>Action</th>
+          </tr>
+        </thead>
+        <tbody>
+          {employees.map((employee) => {
+            return (
+              <tr key={employee.id}>
+                <th>{employee.id}</th>
+                <td>{employee.fullName}</td>
+                <td>{employee.email}</td>
+                <td>{employee.position}</td>
+                <td className="flex flex-col gap-1">
+                  <EditButton employee={employee} />
+                  <DeleteButton
+                    employeeId={employee.id}
+                    employeeName={employee.fullName}
+                  />
+                </td>
+              </tr>
+            );
+          })}
+        </tbody>
+      </table>
+    </div>
+  );
+}
